1b641ff35b1fe85e8dd990e6d6c4433d0aa9e36b
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-02-26 15:22:55 +0900

    ロジックとビューを分離

--- bbs.php
+++ bbs.php
@@ -49,6 +49,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
+// 投稿された内容を取得するSQLを作成して結果を取得
+$sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
+$result = mysqli_query($link, $sql);
+
+// 取得した結果を$postsに格納
+$post = array();
+
+if ($result !== false && mysqli_num_rows($result)) {
+    while ($post = mysqli_fetch_assoc($result)) {
+        $posts[] = $post;
+    }
+}
+
+// 取得結果を解放して接続を閉じる
+mysqli_free_result($result);
+mysqli_close($link);
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -77,28 +94,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <input type="submit" value="送信" name="submit">
     </form>
 
-    <?php
-    // 投稿された内容を取得するSQLを作成して結果を取得
-    $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
-    $result = mysqli_query($link, $sql);
-    ?>
-    <?php if ($result !== false && mysqli_num_rows($result)) : ?>
+
+    <?php if (count($posts) > 0) : ?>
         <ul>
-            <?php while ($post = mysqli_fetch_assoc($result)) : ?>
+            <?php foreach($posts as $post) : ?>
                 <li>
                     <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>：
                     <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
                     - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
                 </li>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
         </ul>
     <?php endif; ?>
 
-    <?php
-    // 取得結果を解放して接続を閉じる
-    mysqli_free_result($result);
-    mysqli_close($link);
-    ?>
+
 </body>
 
 </html>
